8d0f8ab0114705d3fe37ea93e08d2757c89510aa
Quanluis <quanluis@protonmail.com>
2019-08-21 10:42:01 -0400

    created addition lines of strings to simple cmd terminal program.

--- helloyou.cpp
+++ helloyou.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main () //main function
 { 
-    std::cout <<"Hello world\n";
+cout << "Hello alien\n\n\n" << endl;
+cout << "This was made by luis" << endl;
+     cout << "The cow jumped over the moon\n"
+     << "The spoon followed." << endl;
     return 0;
 }
