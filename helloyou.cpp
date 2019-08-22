f5aac6b23915196993d0622c6a0929c50208d27a
Quanluis <quanluis@protonmail.com>
2019-08-22 11:35:55 -0400

    Created a simple if else statement in c++

--- helloyou.cpp
+++ helloyou.cpp
@@ -7,5 +7,20 @@ cout << "Hello alien\n\n\n" << endl;
 cout << "This was made by luis" << endl;
      cout << "The cow jumped over the moon\n"
      << "The spoon followed." << endl;
+
+    int tomAge; 
+
+    int rickAge;
+
+    tomAge = 18; rickAge = 19;
+
+    cout << tomAge << endl; 
+
+    if(tomAge == rickAge){
+        cout << "Same age";
+    } else {
+        cout << "not the same age.";
+    };
+
     return 0;
 }
